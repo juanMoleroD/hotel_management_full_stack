b74d7a24050af33c174f3ad89770be3b9c392b06
juanMoleroD <juanldimeco@gmail.com>
2022-08-31 11:54:15 +0100

    completed frontend basic wireframe with placeholder text. Still very rought

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import BookingContainer from './container/BookingContainer';
 
 function App() {
   return (
     <div className="App">
-      
+        <BookingContainer />
     </div>
   );
 }
---
+++ client/src/container/BookingContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const BookingContainer = () => {
+    return (
+        <>
+            <header>
+                <h1>Booking management system</h1>
+            </header>
+            <main>
+                <h2>All Bookings</h2>
+                <section>
+                    <p>this is a booking</p>
+                    <p>this is a booking</p>
+                    <p>this is a booking</p>
+                    <p>this is a booking</p>
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default BookingContainer;
