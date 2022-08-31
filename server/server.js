da7281c4ccd92bc3927ee7213c1916d91fa6ab1c
juanMoleroD <juanldimeco@gmail.com>
2022-08-31 10:56:16 +0100

    added get method

--- server/server.js
+++ server/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 
 app.listen(9000, function() {
     console.log(`Listening to port ${this.address().port}`);
 })
+
+app.get('/', (request, response) => {
+    response.json({"message": "hello world"})
+})
+
+
