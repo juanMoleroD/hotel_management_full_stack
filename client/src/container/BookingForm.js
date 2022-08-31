0203c1f617b45db5249f6be104ac281e5f7704c5
juanMoleroD <juanldimeco@gmail.com>
2022-08-31 15:18:24 +0100

    completed functionality for post and delete, tested successfully. '

--- client/src/container/BookingForm.js
+++ client/src/container/BookingForm.js
@@ -2,33 +2,47 @@ import React, {useState} from "react";
 
 const BookingForm = ({getBookings}) => {
 
-    const [newBooking, setNewBooking] = useState("");
+    const [newBooking, setNewBooking] = useState({
+        guestName: "",
+        guestEmail: "",
+        guestStatus: "Pending"
+    });
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
-        const payload = {
-            name: newBooking
-        }
         fetch("http://localhost:9000/api/bookings", {
             method: "Post",
-            body: JSON.stringify(payload),
+            body: JSON.stringify(newBooking),
             headers: { 'Content-Type': 'application/json' }
         })
-        .then(() => getBookings());
-        setNewBooking("");
+        .then(() => {
+            getBookings();
+            setNewBooking({
+                guestName: "",
+                guestEmail: "",
+                guestStatus: "Pending"
+            })
+        });
     }
 
     const handleTextInput = (event) => {
-        setNewBooking(event.target.value);
+        const newFormInput = Object.assign({}, newBooking);
+        newFormInput[event.target.name] = event.target.value;
+        setNewBooking(newFormInput);
     }
 
     return( 
         <section id="booking-form-section">
             <h2>Add Booking</h2>
             <form id="booking-form" onSubmit={handleFormSubmit}>
-                <label>Booking name</label>
-                <input type="text" value={newBooking} onChange={handleTextInput}/>
-                <input type="submit" value="send"/>
+                <label>Guestname</label>
+                <input type="text" name="guestName" value={newBooking.guestName} onChange={handleTextInput}/>
+                <label>Guest Email</label>
+                <input type="text" name="guestEmail" value={newBooking.guestEmail} onChange={handleTextInput}/>
+                <label>Status</label>
+                <input type="text" name="guestStatus" value={newBooking.guestStatus} onChange={handleTextInput}/>
+                <br/>
+                <input type="submit" value="ADD BOOKING"/>
             </form>
         </section>
     )
