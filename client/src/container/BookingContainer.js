1d307309b3cdd656a21130322414463b16b4717a
juanMoleroD <juanldimeco@gmail.com>
2022-08-31 11:59:24 +0100

    completed basic layout

--- client/src/container/BookingContainer.js
+++ client/src/container/BookingContainer.js
@@ -7,12 +7,24 @@ const BookingContainer = () => {
                 <h1>Booking management system</h1>
             </header>
             <main>
+                <section>
+                    <h2>Add Booking</h2>
+                    <form>
+                        <label>Booking number</label>
+                        <input type="text"/>
+                        <input type="submit" value="send"/>
+                    </form>
+                </section>
                 <h2>All Bookings</h2>
                 <section>
                     <p>this is a booking</p>
+                    <button>Delete</button>
                     <p>this is a booking</p>
+                    <button>Delete</button>
                     <p>this is a booking</p>
+                    <button>Delete</button>
                     <p>this is a booking</p>
+                    <button>Delete</button>
                 </section>
             </main>
         </>
